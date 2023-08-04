abc136c8d7bd3efb597f3b3150861fd9a896d964
Aditi <aditipandeya1608@gmail.com>
2023-08-04 10:55:11 +0530

    Tried again

--- server.py
+++ server.py
@@ -1,34 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello World', 200
+    return 'Hello World'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.get_json()
-    if 'first' in data and 'second' in data:
-        first = data['first']
-        second = data['second']
-        result = first + second
-        response = {'result': result}
-        return jsonify(response), 200
-    else:
-        return jsonify({'error': 'Invalid request. Please provide both "first" and "second" numbers in the request body.'}), 400
+    a = int(request.form.get('a'))
+    b = int(request.form.get('b'))
+    return str(a + b)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.get_json()
-    if 'first' in data and 'second' in data:
-        first = data['first']
-        second = data['second']
-        result = first - second
-        response = {'result': result}
-        return jsonify(response), 200
-    else:
-        return jsonify({'error': 'Invalid request. Please provide both "first" and "second" numbers in the request body.'}), 400
+    a = int(request.form.get('a'))
+    b = int(request.form.get('b'))
+    return str(a - b)
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
