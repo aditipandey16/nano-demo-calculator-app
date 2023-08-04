ccb7ee58dca1b30b715e5b49ac9291706e8d0c58
Aditi <aditipandeya1608@gmail.com>
2023-08-04 10:51:23 +0530

    Tried

--- server.py
+++ server.py
@@ -1,19 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello World'
+    return 'Hello World', 200
 
 @app.route("/calculator/add", methods=['POST'])
-def add(a,b):
-    return 'a+b'
+def add():
+    data = request.get_json()
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first + second
+        response = {'result': result}
+        return jsonify(response), 200
+    else:
+        return jsonify({'error': 'Invalid request. Please provide both "first" and "second" numbers in the request body.'}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
-def subtract(a,b):
-    return 'a-b'
+def subtract():
+    data = request.get_json()
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first - second
+        response = {'result': result}
+        return jsonify(response), 200
+    else:
+        return jsonify({'error': 'Invalid request. Please provide both "first" and "second" numbers in the request body.'}), 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=5000, host='0.0.0.0')
